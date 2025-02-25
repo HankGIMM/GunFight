474ebd3d51b7a6208e9362cf91d2418a5218fa60
HankGIMM <sidewixer@gmail.com>
2025-02-25 02:38:03 -0700

    updated playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        verticalRotation -= mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Handle movement
+        float moveForwardBackward = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float moveLeftRight = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        Vector3 move = transform.right * moveLeftRight + transform.forward * moveForwardBackward;
+        transform.position += move;
     }
 }
